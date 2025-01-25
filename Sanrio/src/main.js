273cf869999be6e7d9daa6189ea81cdc57761574
Parkssujin <96673950+Parkssujin@users.noreply.github.com>
2025-01-25 19:37:53 +0900

    Sanrio-Add

---
+++ Sanrio/src/main.js
@@ -0,0 +1,50 @@
+// JSON 파일에서 항목들을 가져오기
+function loadItems() {
+    return fetch('data/data.json')
+      .then(response => response.json())
+      .then(json => json.items);
+  }
+  
+  //  리스트 업데이트하기
+  function displayItems(items) {
+    const container = document.querySelector('.items');
+    container.innerHTML = items.map(item => createHTMLString(item)).join('');
+  }
+  
+  // 주어진 데이터 항목으로 HTML 리스트 아이템 생성하기
+  function createHTMLString(item) {
+    return `
+      <li class="item">
+          <img src="${item.image}" alt="${item.type}" class="item_thumbnail" />
+          <span class="item_description">${item.name}, ${item.since}</span>
+      </li>
+      `;
+  }
+  
+  function onButtonClick(event, items) { 
+    const dataset = event.target.dataset;
+    const key = dataset.key;
+    const value = dataset.value;
+  
+    if (key == null || value == null) {
+      return;
+    }
+  
+    displayItems(items.filter(item => item[key] === value));
+  }
+  
+  function setEventListeners(items) {
+    const logo = document.querySelector('.logo');
+    const buttons = document.querySelector('.buttons');
+    logo.addEventListener('click', () => displayItems(items));
+    buttons.addEventListener('click', event => onButtonClick(event, items));
+  }
+  
+  // main
+  loadItems()
+    .then(items => {
+      displayItems(items);
+      setEventListeners(items);
+    })
+    .catch(console.log);
+  
